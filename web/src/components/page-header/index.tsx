5e89f2f4ba5a09ed15cd088a076b68d3bba699e3
Leo Borba <leonardo.borba@renault.com>
2020-09-17 17:41:30 -0300

    header teacher list

--- web/src/components/page-header/index.tsx
+++ web/src/components/page-header/index.tsx
@@ -22,9 +22,9 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
             <div className="header-content">
                 <strong>{props.title}</strong>
+                {props.children}
             </div>
             
-            {props.children}
         </header>
     )
 }
--- web/src/pages/teacher-list/index.tsx
+++ web/src/pages/teacher-list/index.tsx
@@ -9,7 +9,20 @@ function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis">
-                teste
+                <form id="search-teachers" action="">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" id="subject"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="wek-day">Dia da semana</label>
+                        <input type="text" id="wek-day"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="time">Horario</label>
+                        <input type="text" id="time"/>
+                    </div>
+                </form>
             </PageHeader>
         </div>
     )
